8f02b19f2537885d10e8b0d8db379ccc7cab452e
Mitchell Stahl <mbs22f@fsu.edu>
2024-05-18 15:52:49 -0400

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Route, Router, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
