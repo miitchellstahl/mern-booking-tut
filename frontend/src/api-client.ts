9780fddc35a8cab2065530a6eec1fbd767340af7
Mitchell Stahl <mbs22f@fsu.edu>
2024-05-25 13:48:37 -0400

    Added view hotels functionality

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -1,6 +1,6 @@
 import { RegisterFormData } from "./pages/Register";
 import { SignInFormData } from "./pages/SignIn";
-import { HotelType } from "../../backend/src/shared/types";
+import { HotelType } from "./types/types";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
 
